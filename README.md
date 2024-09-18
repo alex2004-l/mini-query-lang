c65c6a32157a13b71a6c0a77f0ca126e435c556b
Lache Alexandra Florentina Georgiana <alexandralache0802@yahoo.com>
2024-09-18 22:04:05 +0300

    Added source files and README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+Mini Query Language
+---
+Implementation of a query language inspired by SQL _using functional programming paradigms_ with a _custom-build database in Scala_.
+
+---
+### Implementation
+
+- ``Table operations``: insert row, delete row, sort rows, select columns, filter rows, update row.
+- ``Database operations``: create, drop, select, join.
+- ``Query language``: an API that allows SQL-like operations on tables and databases(__PP_SQL_DB__ and __PP_SQL_Table__). For readability, in this project are defined implicit conversions and operators.
+---
+### Usage
+
+Usage examples can be founde __[here](src/main/scala/Queries.scala)__.
+
+---
---
+++ src/main/scala/Queries.scala
@@ -0,0 +1,30 @@
+object Queries {
+
+  def killJackSparrow(t: Table): Option[Table] = {
+    // se filtreaza dupa coloana nume; se verifica ca numele sa nu fie "Jack"
+    queryT(PP_SQL_Table_Filter(Some(t), "FILTER", Field("name", !_.equals("Jack"))))
+  }
+
+  def insertLinesThenSort(db: Database): Option[Table] = {
+    // creeaza si selecteaza un nou tabel, insereaza o lista de randuri in acesta, dupa care o sorteaza
+    queryT((queryT((Some(queryDB((
+      queryDB((Some(db), "CREATE", "Inserted Fellas"))
+      , "SELECT", List("Inserted Fellas"))).get(0)), "INSERT", List(
+      Map("name" -> "Ana", "age" -> "93", "CNP" -> "455550555"),
+      Map("name" -> "Diana", "age" -> "33", "CNP" -> "255532142"),
+      Map("name" -> "Tatiana", "age" -> "55", "CNP" -> "655532132"),
+      Map("name" -> "Rosmaria", "age" -> "12", "CNP" -> "855532172")
+    ))), "SORT", "age"))
+  }
+
+  def youngAdultHobbiesJ(db: Database): Option[Table] = {
+    // Combina cele doua tabele, filtreaza tabela rezultat dupa cele trei criterii si extrage
+    // coloanele "name" si "hobby"
+    queryT((queryT((queryT((queryT((Some(queryDB((
+          Some(db), "JOIN", "People", "name", "Hobbies", "name")).get(0)),
+          "FILTER", Field("age", str => !str.equals("") && str.toInt < 25))),
+        "FILTER", Field("name", _.startsWith("J")))),
+      "FILTER", Field("hobby", !_.equals("")))),
+      "EXTRACT", List("name", "hobby")))
+  }
+}
